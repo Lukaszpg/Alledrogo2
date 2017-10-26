742e8d2c4945152e5c797731360d001952571ef9
Łukasz <lukasz.p.gorny@gmail.com>
2017-10-26 15:19:30 +0200

    Added password strength meter

--- src/main/resources/static/js/registration.js
+++ src/main/resources/static/js/registration.js
@@ -2,4 +2,24 @@ $(document).ready(function () {
     $('#password').tooltip({
             html: true
         });
+
+    $('#password').password({
+        shortPass: 'Hasło jest za krótkie',
+        badPass: 'Spróbuj dodać liczby',
+        goodPass: 'Spróbuj dodać znaki specjalne',
+        strongPass: 'Silne hasło',
+        containsUsername: 'The password contains the username',
+        enterPass: 'Wpisz swoje hasło',
+        showPercent: true,
+        showText: true, // shows the text tips
+        animate: true, // whether or not to animate the progress bar on input blur/focus
+        animateSpeed: 'fast', // the above animation speed
+        username: false, // select the username field (selector or jQuery instance) for better password checks
+        usernamePartialMatch: true, // whether to check for username partials
+        minimumLength: 8 // minimum password length (below this threshold, the score is 0)
+    });
+
+    $('#password').focusin(function() {
+        $('#passwordValidationError').addClass("hide");
+    });
 });
